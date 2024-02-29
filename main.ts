ff5d8a572e0cd988d8db9500ef4cf6ed5ca34462
NorePeeters <157713696+NorePeeters@users.noreply.github.com>
2024-02-29 14:56:57 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(1)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(1)
+})
+basic.forever(function () {
+    basic.showNumber(0)
+})
