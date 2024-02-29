57530168b0cdb4cffe01934eef921bfb4cc70f66
NorePeeters <157713696+NorePeeters@users.noreply.github.com>
2024-02-29 15:05:16 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-input.onButtonPressed(Button.A, function () {
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Right, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
     basic.showNumber(1)
 })
-input.onButtonPressed(Button.B, function () {
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Left, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
     basic.showNumber(1)
 })
 basic.forever(function () {
