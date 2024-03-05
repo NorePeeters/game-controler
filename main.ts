ff0d3833db0f6da4919c76e3c92001eceb30d9a1
NorePeeters <157713696+NorePeeters@users.noreply.github.com>
2024-03-05 11:16:49 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,11 +1,7 @@
-Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Right, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
-    basic.showNumber(1)
-    basic.pause(2000)
-    basic.showNumber(0)
+basic.forever(function () {
+    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Left) || Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Right)) {
+        basic.showNumber(1)
+    } else {
+        basic.showNumber(0)
+    }
 })
-Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Left, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
-    basic.showNumber(1)
-    basic.pause(2000)
-    basic.showNumber(0)
-})
-basic.showNumber(0)
